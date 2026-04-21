8581a99d5b571c95b86bfee99b3807ed7c125f79
JunichiHattori <junhat6@gmail.com>
2026-04-21 18:54:06 +0900

    tailwind daisyUI導入

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-nesting'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 }
